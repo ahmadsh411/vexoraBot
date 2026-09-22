404edcecfd96a5e80aaaeb21900a4788e771cde8
ahmadsh411 <eng.ahmad.shehade@gmail.com>
2026-09-23 01:08:57 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,23 @@
 
 use App\Http\Controllers\WheelApiController;
 use Illuminate\Support\Facades\Route;
+use SergiX44\Nutgram\Nutgram;
 
 Route::prefix('wheel')->group(function () {
     Route::post('/state', [WheelApiController::class, 'state']);
     Route::post('/spin', [WheelApiController::class, 'spin']);
+});
+
+
+
+Route::get('/setup-webhook', function (Nutgram $bot) {
+    $bot->deleteWebhook();
+    $url = 'https://vexora-backend-0j8z.onrender.com/api/telegram/webhook';
+    $bot->setWebhook($url);
+
+    return response()->json([
+        'status' => 'success',
+        'message' => 'تم تفعيل الـ Webhook بنجاح!',
+        'url' => $url
+    ]);
 });
