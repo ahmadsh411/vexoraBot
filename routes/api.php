aeca379dc7540887aad30a549efa4cde277cbe15
ahmadsh411 <eng.ahmad.shehade@gmail.com>
2026-09-22 20:34:48 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,28 @@
 
 use App\Http\Controllers\WheelApiController;
 use Illuminate\Support\Facades\Route;
+use SergiX44\Nutgram\Nutgram;
 
 Route::prefix('wheel')->group(function () {
     Route::post('/state', [WheelApiController::class, 'state']);
     Route::post('/spin', [WheelApiController::class, 'spin']);
+});
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Telegram Bot Webhook Route
+|--------------------------------------------------------------------------
+*/
+Route::post('/telegram/webhook', function (Nutgram $bot) {
+    
+    // أمر /start
+    $bot->onCommand('start', function (Nutgram $bot) {
+        $bot->sendMessage('أهلاً بك في بوت VEXORA! البوت يعمل الآن بنجاح 🎉');
+    });
+
+    // معالجة باقي الطلبات
+    $bot->run();
 });
